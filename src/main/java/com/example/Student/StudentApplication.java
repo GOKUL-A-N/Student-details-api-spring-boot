a478b0c9dff374ab5bf9b759311ae4b873250780
GOKUL-A-N <gokulcricketer141103@gmail.com>
2025-01-12 11:44:12 +0530

    added env file

--- src/main/java/com/example/Student/StudentApplication.java
+++ src/main/java/com/example/Student/StudentApplication.java
@@ -2,11 +2,19 @@ package com.example.Student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class StudentApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+
+		String dbUrl = dotenv.get("MONGO_DB_URL");
+
+		System.setProperty("MONGO_DB_URL",dbUrl);
+
 		SpringApplication.run(StudentApplication.class, args);
 	}
 
